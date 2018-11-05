aec914ebc97687db9eb72a31e507558e07c17aa6
klcatha <klcatha0518@gmail.com>
2018-11-05 15:59:37 +0900

    my count down timer class

--- app/src/main/java/com/example/kengomaruyama/mycountdowntimer/MainActivity.kt
+++ app/src/main/java/com/example/kengomaruyama/mycountdowntimer/MainActivity.kt
@@ -1,9 +1,30 @@
 package com.example.kengomaruyama.mycountdowntimer
 
+import android.icu.util.DateInterval
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.CompletableFuture
 
 class MainActivity : AppCompatActivity() {
+
+    inner class MyCountDownTimer(millisInFuture: Long,
+                                 countDownInterval: Long) :
+    CountDownTimer(millisInFuture, countDownInterval) {
+
+        val isRunning = false
+
+        override fun onTick(millisUntilFinished: Long) {
+            val minute = millisUntilFinished / 1000L / 60L
+            val second = millisUntilFinished / 1000L % 60L
+            timerText.text = "%1d:%2$02d".format(minute, second)
+        }
+
+        override fun onFinish() {
+            timerText.text = "0:00"
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
